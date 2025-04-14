d5f44eb1eadbfe48327d6410df86979a535aea58
GhanimAlmansouri <ghanim.almansouri@gmail.com>
2025-04-14 18:23:34 +0100

    add auth tests w/mistakes edited

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -14,13 +14,13 @@ func TestGetAPIKey(t *testing.T) {
 	}{
 		{
 			name:           "valid Authorizarion Header",
-			headers:        http.Header{"Authorization": []string{"Bearer notsorandom"}},
+			headers:        http.Header{"Authorization": []string{"ApiKey notsorandom"}},
 			expectedAPIkey: "notsorandom",
 			expectedError:  false,
 		},
 		{
 			name:           "invalid Authorization format",
-			headers:        http.Header{"Authorization": []string{"notsorandom"}},
+			headers:        http.Header{"Authorization": []string{"Bearer notsorandom"}},
 			expectedAPIkey: "notsorandom",
 			expectedError:  true,
 		},
@@ -29,13 +29,13 @@ func TestGetAPIKey(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			apiKey, err := GetAPIKey(tc.headers)
-			if tc.name == "valid Authorization Header" && tc.expectedError && err != nil {
+			if tc.expectedError == false && err != nil {
 				t.Fatalf("Error: %s", err)
 			}
-			if tc.name != "invalid Authorization format" && !tc.expectedError && err == nil {
+			if tc.expectedError == true && err == nil {
 				t.Fatal("Expected an error, received none.")
 			}
-			if tc.name != "invalid Authorization format" && tc.expectedError && apiKey != tc.expectedAPIkey {
+			if tc.expectedError == false && apiKey != tc.expectedAPIkey {
 				t.Fatalf("Unexpected API key: Expected %s but got %s", tc.expectedAPIkey, apiKey)
 			}
 
